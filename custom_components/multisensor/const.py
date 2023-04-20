c26838abdf280030a024d9ce2ae9324fff5237d9
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-20 20:54:13 +0800

    Update const.py

--- custom_components/multisensor/const.py
+++ custom_components/multisensor/const.py
@@ -1,19 +1,28 @@
-from homeassistant.const import(
-    PLATFORM
-)
+"""Constants for the component."""
 
-from homeassistant.components.sensor import SensorDeviceClass
+# Component domain, used to store component data in hass data.
+DOMAIN = "Isensit"
 
-DOMAIN = "multisensor"
+# == ThermIQ Const
+CONF_ID = "id_name"
+CONF_MQTT_NODE = "mqtt_node"
+CONF_MQTT_DBG = "thermiq_dbg"
+CONF_MQTT_HEX = "hexformat"
+CONF_LANGUAGE = "language"
+DEFAULT_NODE = "ThermIQ/ThermIQ-mqtt"
+CONF_DATA = "data_msg"
+DEFAULT_DATA = "/data"
+CONF_CMD = "cmd_msg"
+DEFAULT_CMD = "/write"
+DEFAULT_DBG = False
+AVAILABLE_LANGUAGES = ["en", "se", "fi", "no", "de"]
 
-DEFAULT_NAME = "VTEC"
 
-SENSOR_ID = "Sensor ID"
-
-Platform = [PLATFORM.SENSOR]
-
-
-
-SENSOR = {
-    'pm10': ['Particulate matter(PM10)']
-}
+PLATFORM_AUTOMATION = "automation"
+PLATFORM_BINARY_SENSOR = "binary_sensor"
+PLATFORM_GROUP = "group"
+PLATFORM_INPUT_BOOLEAN = "input_boolean"
+PLATFORM_INPUT_NUMBER = "input_number"
+PLATFORM_INPUT_SELECT = "input_select"
+PLATFORM_INPUT_TEXT = "input_text"
+CONF_ENTITY_PLATFORM = "entity_platform"
