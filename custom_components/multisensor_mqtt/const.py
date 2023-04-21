e54aa0e85e87e89efe642404963a05e0f140d93b
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-21 20:02:45 +0800

    Update const.py

--- custom_components/multisensor_mqtt/const.py
+++ custom_components/multisensor_mqtt/const.py
@@ -1,7 +1,7 @@
 """Constants for the component."""
 
 # Component domain, used to store component data in hass data.
-DOMAIN = "thermiq_mqtt"
+DOMAIN = "Isensit"
 
 # == ThermIQ Const
 CONF_ID = "id_name"
